34ede3cc4893b7b33a89096ebb7bd6c1c0c2f013
Julien Ganichot <ganitzsh@Juliens-MBP.fritz.box>
2017-01-25 20:50:31 +0100

    Added sleep to wait till ready to run.sh

--- deployment/run.sh
+++ deployment/run.sh
@@ -4,6 +4,8 @@ if [ ! -d "data" ]; then
 fi
 (docker-compose stop ; docker-compose up -d)
 if [ $? -eq 0 ]; then
+    echo "Waiting 20 seconds to be ready..."
+    sleep 20
     echo "API Running on port 4242"
     echo "Postgres database available on port 5431"
 else
