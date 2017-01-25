70e78a4a6c7488f692494622a8e64b1837af8191
Julien Ganichot <ganitzsh@Juliens-MBP.fritz.box>
2017-01-25 20:40:57 +0100

    Added run.sh script + Renamed jar to rssagg.jar

---
+++ deployment/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+if [ ! -d "data" ]; then
+  (mkdir data)
+fi
+(docker-compose stop ; docker-compose up -d)
+if [ $? -eq 0 ]; then
+    echo "API Running on port 4141"
+    echo "Postgres database available on port 5431"
+else
+    echo "An error occured"
+fi
