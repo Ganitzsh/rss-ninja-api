cb7a8f656d0da5db29e599db705be8f7b7992b83
Julien Ganichot <ganitzsh@Juliens-MBP.fritz.box>
2017-01-27 23:12:36 +0100

    Changed message when CTCheck fails

--- src/main/java/filters/CTCheck.java
+++ src/main/java/filters/CTCheck.java
@@ -18,8 +18,24 @@ public class CTCheck implements Filter {
         Result result;
 
         if (!context.isRequestJson()) {
-            return Results.unauthorized().json().render(new JSendResp(401, new Exception("Check your Content-Type, only application/json or application/xml allowed")));
+            return Results.unauthorized().json().render(new JSendResp(401, new SimpleError("Check your Content-Type, only application/json or application/xml allowed")));
         }
         return filterChain.next(context);
+    }
+
+    public class SimpleError {
+        public String message;
+
+        public SimpleError(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
     }
 }
